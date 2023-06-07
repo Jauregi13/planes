12b9fe6cb78fb8edcef5dded6946e6cdac56a621
jauregi13 <jon.j1305@gmail.com>
2023-06-07 02:45:08 +0200

    Pantalla principal terminada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCol, IonContent, IonGrid, IonHeader, IonMenu, IonPage, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import logo from '../../public/assets/logo.jpeg';
+import comida from '../../public/assets/comida.svg';
+import ruteo from '../../public/assets/ruteo.svg';
+import cine from '../../public/assets/cine.svg';
+
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonGrid>
+          <IonRow className='ion-justify-content-center ion-align-items-center'>
+            <IonCol size='auto'>
+            <img className='ion-margin-top' src={logo} alt='logo' width={240} height={240}></img>
+            </IonCol>
+          </IonRow>
+          <IonRow className='ion-justify-content-center ion-align-items-center ion-margin-top'>
+            <IonCol size='auto'>
+              <IonText className='home-text'>¿Qué planes tienes para hoy?</IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow className='ion-justify-content-center ion-align-items-center ion-margin-top'>
+            <IonCol size='auto'>
+              <img src={comida} width={160} height={160}></img>
+            </IonCol>
+            <IonCol size='auto'>
+              <img src={ruteo} width={160} height={160}></img>
+            </IonCol>
+          </IonRow>
+          <IonRow className='ion-justify-content-center ion-align-items-center'>
+            <IonCol size='auto'>
+              <img src={cine} width={160} height={160}></img>
+            </IonCol>
+          </IonRow> 
+        </IonGrid>
+        
       </IonContent>
     </IonPage>
   );
