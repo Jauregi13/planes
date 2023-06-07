162322c6316d5331f61aaaf32c34afbf49932a3e
jauregi13 <jon.j1305@gmail.com>
2023-06-07 22:31:09 +0200

    Añadir pagina comidas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,6 @@
-import { IonCol, IonContent, IonGrid, IonHeader, IonMenu, IonPage, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCol, IonContent, IonGrid, IonPage, IonRow, IonText } from '@ionic/react';
+import { NavLink } from 'react-router-dom';
 import './Home.css';
-import logo from '../../public/assets/logo.png';
-import comida from '../../public/assets/images/comida.svg';
-import ruteo from '../../public/assets/images/ruteo.svg';
-import cine from '../../public/assets/images/cine.svg';
-
 
 const Home: React.FC = () => {
   return (
@@ -14,7 +9,7 @@ const Home: React.FC = () => {
         <IonGrid>
           <IonRow className='ion-justify-content-center ion-align-items-center'>
             <IonCol size='auto'>
-            <img className='ion-margin-top' src={logo} alt='logo' width={240} height={240}></img>
+            <img className='ion-margin-top' src='/assets/logo.png' alt='logo' width={240} height={240}></img>
             </IonCol>
           </IonRow>
           <IonRow className='ion-justify-content-center ion-align-items-center ion-margin-top'>
@@ -24,15 +19,17 @@ const Home: React.FC = () => {
           </IonRow>
           <IonRow className='ion-justify-content-center ion-align-items-center ion-margin-top'>
             <IonCol size='auto'>
-              <img src={comida} width={160} height={160}></img>
+              <img src='/assets/images/comida.svg' width={160} height={160}>
+                <NavLink to='/restaurantes'></NavLink>
+              </img>
             </IonCol>
             <IonCol size='auto'>
-              <img src={ruteo} width={160} height={160}></img>
+              <img src='/assets/images/ruteo.svg' width={160} height={160}></img>
             </IonCol>
           </IonRow>
           <IonRow className='ion-justify-content-center ion-align-items-center'>
             <IonCol size='auto'>
-              <img src={cine} width={160} height={160}></img>
+              <img src='/assets/images/cine.svg' width={160} height={160}></img>
             </IonCol>
           </IonRow> 
         </IonGrid>
---
+++ src/pages/comida/menuTabs.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { IonApp, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Route, Redirect } from 'react-router';
+
+import { restaurant, heart, pizza} from 'ionicons/icons';
+
+
+const MenuTabs: React.FC = () => {
+  return (
+    <IonApp>
+        <IonReactRouter>
+            <IonTabs>
+                <IonRouterOutlet>
+                    <Route exact path="/restaurantes">
+                        
+                    </Route>
+                    <Route exact path="/recetas">
+                        
+                    </Route>
+                    <Route path="/restFavoritos">
+                        
+                    </Route>
+                </IonRouterOutlet>
+                <IonTabBar slot="bottom">
+                    <IonTabButton tab="restaurantes" href="/restaurantes">
+                        <IonIcon aria-hidden="true" icon={pizza} />
+                        <IonLabel>Restaurantes</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="recetas" href="/recetas">
+                        <IonIcon aria-hidden="true" icon={restaurant} />
+                        <IonLabel>Recetas</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="favoritos" href="/restFavoritos">
+                        <IonIcon aria-hidden="true" icon={heart} />
+                        <IonLabel>Favoritos</IonLabel>
+                    </IonTabButton>
+                </IonTabBar>
+            </IonTabs>
+        </IonReactRouter>
+    </IonApp>
+  );
+};
+
+export default MenuTabs;
