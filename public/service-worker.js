d5e14578c6d953baf29e582b41f000ab8db0950d
jauregi13 <jon.j1305@gmail.com>
2023-06-08 02:28:40 +0200

    Intentar corregir errores de carga imagenes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('my-cache').then(cache => {
-        cache.addAll(['/', '/index.html', '/manifest.json','/assets']); // Agrega las rutas de los archivos estáticos que deseas almacenar en caché
+        cache.addAll(['/', '/index.html', '/manifest.json','/assets/*']); // Agrega las rutas de los archivos estáticos que deseas almacenar en caché
       })
     );
   });
